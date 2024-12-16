a978f3129bba801e9152001869e0777776b8bbc8
Shane <shanejen56@gmail.com>
2024-12-16 13:55:43 -0800

    cleaned up API calls, started functionality on hourly weather

--- api.js
+++ api.js
@@ -1,6 +1,7 @@
 // calls NWS API (twice), sends data to render.js
 
-import { render } from "./render.js";
+import { one_week_forecast } from "./render.js";
+import { daily_forecast } from "./render.js";
 
 export function call_api(coordinates) { 
     // function that is called in main.js
@@ -18,17 +19,17 @@ function first_call(url) {
         return response.json();
     })
     .then(data => {
-        second_call(data);
-        // send current data JSON to another API call to access more nested data
-        // NWS API docs say to call the API twice but I'm not entirely sure if this is even
-        // the right way to do it? 
+        // NWS API requires 2 calls, 1 for main data, and another to access specific 
+        // data, in this case i want forecast (week) and forecastHourly (day)
+        call_for_weekly_forecast(data);
+        call_for_daily_forecast(data);
     })
     .catch(error => {
         console.error("ERROR", error.message);
     })
 }
 
-function second_call(initial_data) {
+function call_for_weekly_forecast(initial_data) {
     let main_url = initial_data.properties.forecast;
     // the main data nested properties --> forecast
 
@@ -41,18 +42,30 @@ function second_call(initial_data) {
         return response.json();
     })
     .then(data => {
-        render(data);
-        // send the final data to render.js to render front-end
+        one_week_forecast(data);
     })
     .catch(error => {
         console.error("ERROR", error.message);
     })
 }
-}
 
-function display_weather(final_data) {
-    for (let i = 0; i < 14; i++) {
-        console.log(`${final_data.periods[i].name} : ${final_data.periods[i].temperature}`);
-        // loops through 14 days, prints day : temperature
-    }
+function call_for_daily_forecast(initial_data) {
+    let main_url = initial_data.properties.forecastHourly;
+    // the main data nested properties --> forecastHourly
+
+    fetch(main_url)
+    .then(response => {
+        if (!response.ok) {
+            alert("ERROR: Out of Bounds or Connection Failure");
+            throw new Error("API ERROR (2nd call)");
+        }
+        return response.json();
+    })
+    .then(data => {
+        daily_forecast(data);
+    })
+    .catch(error => {
+        console.error("ERROR", error.message);
+    })
+}
 }
--- render.js
+++ render.js
@@ -4,24 +4,32 @@
 // variables corresponding to already exisiting HTML elements
 const mainPage = document.getElementById("page");
 const rightOfMap = document.getElementById("right-of-map");
+const leftOfMap = document.getElementById("left-of-map");
 
-export function render(data) {
-    
-    // segmented data for use in for-loop, only 'properties' part of main JSON 
+export function daily_forecast(data) {
     let segmented_data = data.properties;
-    two_week_forecast(segmented_data);
+
+    leftOfMap.innerHTML = '';
+
+    for (let i = 0; i < 12; i++) {
+        const hour_div = document.createElement('div');
+        hour_div.className = "hour-forecast";
+        hour_div.textContent = `${segmented_data.periods[i].temperature}`;
+        leftOfMap.appendChild(hour_div);
+    }
+
 }
 
-function two_week_forecast(data) {
-    // 2 week forecast, starting at 1 (not 0) because we don't need current weather here
+export function one_week_forecast(data) {
+    let segmented_data = data.properties;
 
     rightOfMap.innerHTML = ''; 
     //clears previous HTML, may need to do this a different way in the future
 
-    for (let i = 1; i < 14; i++) {
+    for (let i = 2; i < 14; i++) {
         const day_div = document.createElement('div');
         day_div.className = "day-forecast";
-        day_div.textContent = `${data.periods[i].name} : ${data.periods[i].temperature}`;
+        day_div.textContent = `${segmented_data.periods[i].name} : ${segmented_data.periods[i].temperature}`;
         rightOfMap.appendChild(day_div);
     }
 }
